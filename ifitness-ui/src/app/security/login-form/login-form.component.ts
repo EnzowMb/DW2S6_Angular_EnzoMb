f1a6b736df06f9e6696496f1b75a4b5d3d284fff
Enzo Martinelli Brunozi <89809584+EnzowMb@users.noreply.github.com>
2023-09-28 11:06:16 -0300

    feat: implement AuthService in login

--- ifitness-ui/src/app/security/login-form/login-form.component.ts
+++ ifitness-ui/src/app/security/login-form/login-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
@@ -7,9 +9,10 @@ import { Component } from '@angular/core';
 })
 export class LoginFormComponent {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
-  login(user: string, password: string) {
+  login(user: string, password: string): void {
+    this.auth.login(user, password);
   }
 
 }
