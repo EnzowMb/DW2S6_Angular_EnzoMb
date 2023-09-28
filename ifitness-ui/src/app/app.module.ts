7a41769eea4320070f9ddabf576344b16c60c8ea
Enzo Martinelli Brunozi <89809584+EnzowMb@users.noreply.github.com>
2023-09-28 10:50:16 -0300

    chore: init authService

--- ifitness-ui/src/app/app.module.ts
+++ ifitness-ui/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActivitiesModule } from './activities/activities.module';
 import { SecurityModule } from './security/security.module';
+import { AuthService } from './security/auth.service';
 
 @NgModule({
   declarations: [
@@ -12,11 +14,14 @@ import { SecurityModule } from './security/security.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ActivitiesModule,
     SecurityModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
