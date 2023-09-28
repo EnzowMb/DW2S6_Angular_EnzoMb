d0b61f12024343549aac8f4dfa98047f11c4742b
Enzo Martinelli Brunozi <89809584+EnzowMb@users.noreply.github.com>
2023-09-28 11:21:39 -0300

    feat: implement token

--- ifitness-ui/src/app/security/security.module.ts
+++ ifitness-ui/src/app/security/security.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 
 import { LoginFormComponent } from './login-form/login-form.component';
 
-
 @NgModule({
   declarations: [
     LoginFormComponent
+  ],
+  providers: [
+    JwtHelperService
   ],
   imports: [
     CommonModule,
     FormsModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return '';
+        }
+      }
+    }),
   ],
   exports: [
     LoginFormComponent
